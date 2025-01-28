1e016371d9dde403b3f9bd3028dbbc577f1c4662
Fern <fmenezes098@gmail.com>
2025-01-27 21:43:50 -0300

    :sparkles: Criação da Classe Entidade Tema

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,21 +1,27 @@
-import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn } from 'typeorm';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty } from "class-validator"
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+import { Tema } from "../../tema/entities/tema.entity"
 
-@Entity({ name: 'tb_postagens' })
+@Entity({name: "tb_postagens"})
 export class Postagem {
-  @PrimaryGeneratedColumn()
-  id: number
 
-  @IsNotEmpty()
-  @Column({ length: 100, nullable: false })
-  titulo: string
+    @PrimaryGeneratedColumn()    
+    id: number
 
-  @IsNotEmpty()
-  @Column({ length: 1000, nullable: false })
-  texto: string
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    titulo: string
 
-  @UpdateDateColumn()
-  data: Date
+    @IsNotEmpty()
+    @Column({length: 1000, nullable: false})
+    texto: string
 
-  
+    @UpdateDateColumn()
+    data: Date
+    
+    @ManyToOne(() => Tema, (tema) => tema.postagem, {
+        onDelete: "CASCADE"
+    })
+    tema: Tema
+
 }
